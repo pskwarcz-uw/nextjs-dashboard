f9a5e280f202cc9b7c6d65aa8aa19cb805d50221
Pawel Skwarcz <pskwarcz@icm.edu.pl>
2026-01-14 10:13:16 +0100

    form action should return void

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -34,9 +34,7 @@ export async function createInvoice(formData: FormData) {
   } catch (error) {
     // We'll also log the error to the console for now
     console.error(error);
-    return {
-      message: "Database Error: Failed to Create Invoice.",
-    };
+    //return { message: "Database Error: Failed to Create Invoice." };
   }
 
   revalidatePath("/dashboard/invoices");
@@ -63,7 +61,7 @@ export async function updateInvoice(id: string, formData: FormData) {
   } catch (error) {
     // We'll also log the error to the console for now
     console.error(error);
-    return { message: "Database Error: Failed to Update Invoice." };
+    // return { message: "Database Error: Failed to Update Invoice." };
   }
 
   revalidatePath("/dashboard/invoices");
@@ -72,12 +70,6 @@ export async function updateInvoice(id: string, formData: FormData) {
 
 export async function deleteInvoice(id: string) {
   throw new Error("Failed to Delete Invoice");
-  try {
-    await sql`DELETE FROM invoices WHERE id = ${id}`;
-  } catch (error) {
-    // We'll also log the error to the console for now
-    console.error(error);
-    return { message: "Database Error: Failed to Delete Invoice." };
-  }
+  await sql`DELETE FROM invoices WHERE id = ${id}`;
   revalidatePath("/dashboard/invoices");
 }
